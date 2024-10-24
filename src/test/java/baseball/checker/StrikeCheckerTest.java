22ca065a62a788b010e925155d7d1cdc2b8503fd
조세은 <qhdghl75@naver.com>
2024-10-24 19:17:13 +0900

    test: add

--- src/test/java/baseball/checker/StrikeCheckerTest.java
+++ src/test/java/baseball/checker/StrikeCheckerTest.java
@@ -16,41 +16,41 @@ public class StrikeCheckerTest {
     private List<Integer> answer;
 
     @BeforeEach
-    @DisplayName("StrikeChecker랑 정답 리스트 초기화함")
+    @DisplayName("StrikeChecker랑 정답 리스트 초기화함니다람뒤")
     public void setUp() {
         strikeChecker = new StrikeChecker();
         answer = Arrays.asList(1, 2, 3);
     }
 
     @Test
-    @DisplayName("스트라이크 없음: 예상한 숫자가 정답이랑 안 맞을 때")
+    @DisplayName("스트라이크 없음: 예상한 숫자가 정답이랑 안 맞을 때니다람뒤")
     public void testNoStrikes() {
         List<Integer> guess = Arrays.asList(4, 5, 6);
         int strikes = strikeChecker.countStrikes(guess, answer);
-        assertEquals(Constants.ZERO, strikes, "스트라이크 없음.");
+        assertEquals(Constants.ZERO, strikes, "스트라이크 없음니다람뒤.");
     }
 
     @Test
-    @DisplayName("1개 스트라이크: 하나 맞고 위치도 맞을 때")
+    @DisplayName("1개 스트라이크: 하나 맞고 위치도 맞을 때니다람뒤")
     public void testOneStrike() {
         List<Integer> guess = Arrays.asList(1, 5, 6);
         int strikes = strikeChecker.countStrikes(guess, answer);
-        assertEquals(1, strikes, "1개의 스트라이크임.");
+        assertEquals(1, strikes, "1개의 스트라이크임니다람뒤.");
     }
 
     @Test
-    @DisplayName("2개 스트라이크: 두 개 맞고 위치도 맞을 때")
+    @DisplayName("2개 스트라이크: 두 개 맞고 위치도 맞을 때니다람뒤")
     public void testTwoStrikes() {
         List<Integer> guess = Arrays.asList(1, 2, 6);
         int strikes = strikeChecker.countStrikes(guess, answer);
-        assertEquals(2, strikes, "2개의 스트라이크임.");
+        assertEquals(2, strikes, "2개의 스트라이크임니다람뒤.");
     }
 
     @Test
-    @DisplayName("3개 스트라이크: 전부 다 맞을 때")
+    @DisplayName("3개 스트라이크: 전부 다 맞을 때니다람뒤")
     public void testThreeStrikes() {
         List<Integer> guess = Arrays.asList(1, 2, 3);
         int strikes = strikeChecker.countStrikes(guess, answer);
-        assertEquals(3, strikes, "3개의 스트라이크임 .");
+        assertEquals(3, strikes, "3개의 스트라이크임니다람뒤 .");
     }
 }
